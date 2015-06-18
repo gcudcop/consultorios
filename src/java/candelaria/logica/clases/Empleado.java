f8c886933905fc23739ec2c445202853e3bbeb02
Pato <Pato@Jhonatan>
2015-06-17 20:55:10 -0500

    17/06/2015 Jhonatan Dumancela (Creacion metodos FCliente) 19:48

--- src/java/candelaria/logica/clases/Empleado.java
+++ src/java/candelaria/logica/clases/Empleado.java
@@ -14,7 +14,7 @@ public class Empleado {
   private String nombres_empleado;
   private String apellido_paterno;
   private String apellido_materno;
-  private Integer numero_credencial;
+  private String numero_credencial;
   private String estado_civil;
   private String email_empleado;
   private String telefono_empleado;
@@ -23,7 +23,7 @@ public class Empleado {
     public Empleado() {
     }
 
-    public Empleado(Integer id_empleado, String nombres_empleado, String apellido_paterno, String apellido_materno, Integer numero_credencial, String estado_civil, String email_empleado, String telefono_empleado, String cedula_empleado) {
+    public Empleado(Integer id_empleado, String nombres_empleado, String apellido_paterno, String apellido_materno, String numero_credencial, String estado_civil, String email_empleado, String telefono_empleado, String cedula_empleado) {
         this.id_empleado = id_empleado;
         this.nombres_empleado = nombres_empleado;
         this.apellido_paterno = apellido_paterno;
@@ -39,74 +39,74 @@ public class Empleado {
         return id_empleado;
     }
 
-    public String getNombres_empleado() {
-        return nombres_empleado;
-    }
-
-    public String getApellido_paterno() {
-        return apellido_paterno;
-    }
-
-    public String getApellido_materno() {
-        return apellido_materno;
-    }
-
-    public Integer getNumero_credencial() {
-        return numero_credencial;
-    }
-
-    public String getEstado_civil() {
-        return estado_civil;
-    }
-
-    public String getEmail_empleado() {
-        return email_empleado;
-    }
-
-    public String getTelefono_empleado() {
-        return telefono_empleado;
-    }
-
-    public String getCedula_empleado() {
-        return cedula_empleado;
-    }
-
     public void setId_empleado(Integer id_empleado) {
         this.id_empleado = id_empleado;
+    }
+
+    public String getNombres_empleado() {
+        return nombres_empleado;
     }
 
     public void setNombres_empleado(String nombres_empleado) {
         this.nombres_empleado = nombres_empleado;
     }
 
+    public String getApellido_paterno() {
+        return apellido_paterno;
+    }
+
     public void setApellido_paterno(String apellido_paterno) {
         this.apellido_paterno = apellido_paterno;
+    }
+
+    public String getApellido_materno() {
+        return apellido_materno;
     }
 
     public void setApellido_materno(String apellido_materno) {
         this.apellido_materno = apellido_materno;
     }
 
-    public void setNumero_credencial(Integer numero_credencial) {
+    public String getNumero_credencial() {
+        return numero_credencial;
+    }
+
+    public void setNumero_credencial(String numero_credencial) {
         this.numero_credencial = numero_credencial;
+    }
+
+    public String getEstado_civil() {
+        return estado_civil;
     }
 
     public void setEstado_civil(String estado_civil) {
         this.estado_civil = estado_civil;
     }
 
+    public String getEmail_empleado() {
+        return email_empleado;
+    }
+
     public void setEmail_empleado(String email_empleado) {
         this.email_empleado = email_empleado;
+    }
+
+    public String getTelefono_empleado() {
+        return telefono_empleado;
     }
 
     public void setTelefono_empleado(String telefono_empleado) {
         this.telefono_empleado = telefono_empleado;
     }
 
+    public String getCedula_empleado() {
+        return cedula_empleado;
+    }
+
     public void setCedula_empleado(String cedula_empleado) {
         this.cedula_empleado = cedula_empleado;
     }
-  
+
   
     
 }
