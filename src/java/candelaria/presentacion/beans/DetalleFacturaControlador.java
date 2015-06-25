cd159169ea550d5a461a89648f2fcd09a584cdee
Edison Guerra <eguerravelasco@gmail.com>
2015-06-24 19:39:50 -0500

    Guerra Edison 24/06/2015

--- src/java/candelaria/presentacion/beans/DetalleFacturaControlador.java
+++ src/java/candelaria/presentacion/beans/DetalleFacturaControlador.java
@@ -5,8 +5,13 @@
  */
 package candelaria.presentacion.beans;
 
+import candelaria.logica.clases.Detalle_Factura;
+import candelaria.logica.clases.Producto;
+import candelaria.logica.funciones.FProducto;
+import java.util.ArrayList;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import recursos.Util;
 
 /**
  *
@@ -15,5 +20,64 @@ import javax.faces.bean.ViewScoped;
 @ManagedBean
 @ViewScoped
 public class DetalleFacturaControlador {
-    
+
+    private Detalle_Factura objDetalleFactura;
+    private Detalle_Factura detalleFacturaSel;
+    private ArrayList<Detalle_Factura> lstDetalleFactura;
+    private ArrayList<Producto> lstProductos;
+    private boolean mostrarActualizar;
+
+    public ArrayList<Producto> getLstProductos() {
+        return lstProductos;
+    }
+
+    public void setLstProductos(ArrayList<Producto> lstProductos) {
+        this.lstProductos = lstProductos;
+    }
+
+    public Detalle_Factura getObjDetalleFactura() {
+        return objDetalleFactura;
+    }
+
+    public void setObjDetalleFactura(Detalle_Factura objDetalleFactura) {
+        this.objDetalleFactura = objDetalleFactura;
+    }
+
+    public Detalle_Factura getDetalleFacturaSel() {
+        return detalleFacturaSel;
+    }
+
+    public void setDetalleFacturaSel(Detalle_Factura detalleFacturaSel) {
+        this.detalleFacturaSel = detalleFacturaSel;
+    }
+
+    public ArrayList<Detalle_Factura> getLstDetalleFactura() {
+        return lstDetalleFactura;
+    }
+
+    public void setLstDetalleFactura(ArrayList<Detalle_Factura> lstDetalleFactura) {
+        this.lstDetalleFactura = lstDetalleFactura;
+    }
+
+    public boolean isMostrarActualizar() {
+        return mostrarActualizar;
+    }
+
+    public void setMostrarActualizar(boolean mostrarActualizar) {
+        this.mostrarActualizar = mostrarActualizar;
+    }
+
+    public DetalleFacturaControlador() {
+    }
+
+
+public void cargarProductos(){
+    try {
+            this.lstProductos = FProducto.ObtenerProductos();
+            System.out.println(lstProductos.get(0).getId_producto());
+        } catch (Exception e) {
+            Util.addErrorMessage("private void cargarCategoria dice: " + e.getMessage());
+            System.out.println("private void cargarCategoria dice: " + e.getMessage());
+        }
+}
 }
