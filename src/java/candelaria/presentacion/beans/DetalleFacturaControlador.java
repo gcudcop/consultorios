9e119ef2449280c1f022925ceae43f13e2bfdc6f
Edison Guerra <eguerravelasco@gmail.com>
2015-06-24 19:21:37 -0500

    Guerra Edison 24/06/2015

---
+++ src/java/candelaria/presentacion/beans/DetalleFacturaControlador.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package candelaria.presentacion.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Usuario
+ */
+@ManagedBean
+@ViewScoped
+public class DetalleFacturaControlador {
+    
+}
