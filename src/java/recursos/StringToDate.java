ec5cf4aeb9657b94c31ee268a7a6ba202aa896d6
djdreams <edison_pol@hotmail.com>
2015-11-06 12:21:51 -0500

    geovanny cudco -- locales!!!

--- src/java/recursos/StringToDate.java
+++ src/java/recursos/StringToDate.java
@@ -29,5 +29,22 @@ public class StringToDate {
 
         return auxFecha;
     }
+    
+    public static java.sql.Time devolverHora(Date txtFecha) {
+        java.sql.Time auxFecha = null;
+        String strFecha = new String();
+        try {
+            java.text.SimpleDateFormat sdf1 = new java.text.SimpleDateFormat("hh:mm:ss");
+            strFecha = sdf1.format(txtFecha);
+
+            Date fecha = sdf1.parse(strFecha);
+            java.sql.Time retornoFecha = new java.sql.Time(fecha.getTime());
+            auxFecha = retornoFecha;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return auxFecha;
+    }
 
 }
